6eabb30201dd594e6e2f2970827570c509b2c395
BazDev94 <limoncelloat@gmail.com>
2023-06-04 14:08:33 +0200

    set color

--- alert-map-info/src/app/components/map/map.component.ts
+++ alert-map-info/src/app/components/map/map.component.ts
@@ -11,20 +11,18 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./map.component.scss']
 })
 export class MapComponent implements OnInit {
-  private map:any;
-  regionsLayer: any;
-
+  private map: any;
+  geojsonStyle: any
+  aReg: any[] = ["Liguria", "Puglia", "Sicilia", "Sicilia"]
   constructor(public http: HttpClient) { }
 
   private initMap(): any {
-
-    const geojsonStyle = {
-      fillColor: 'black',
-      fillOpacity: 0.6,
+    this.geojsonStyle = {
+      fillColor: 'grey',
+      fillOpacity: 0.2,
       color: 'black',
       weight: 1
     };
-
     this.map = L.map('map', {
       center: [41.87194, 12.56738],
       zoom: 6,
@@ -33,20 +31,37 @@ export class MapComponent implements OnInit {
       worldCopyJump: false
 
     });
+
     this.http.get<any>('assets/limits_IT_regions.geojson').subscribe(geojsonData => {
-    const regionsLayer = L.geoJSON(geojsonData, {
-        style: geojsonStyle
+      console.log(geojsonData)
+      const regionsLayer = L.geoJSON(geojsonData, {
+        onEachFeature: (feature, layer) => {
+          const pathLayer = layer as L.Path; // Cast esplicito a L.Path
+          const reg_name = feature.properties.reg_name;
+          const prov_name = feature.properties.prov_name;
+          let click = false
+          layer.on('click', (event) => {
+            click = !click
+            if (click) {
+              pathLayer.setStyle({ fillColor: 'green' });
+            } else {
+              pathLayer.setStyle({ fillColor: 'grey' });
+
+            }
+            pathLayer.bindPopup(`Regione: ${reg_name}`).openPopup();
+            console.log(feature)
+          });
+        },
+        style: this.geojsonStyle
       });
       regionsLayer.addTo(this.map);
-
-    });    
+    });
     const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 18,
       minZoom: 3,
       detectRetina: true,
       attribution: 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors',
     });
-
     tiles.addTo(this.map);
     return this.map
   }
